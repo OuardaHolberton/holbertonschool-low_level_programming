a01f7a89650d5aa0f64ad87eca4e58d561739ccd
OuardaHolberton <12985@holbertonstudents.com>
2026-02-25 11:40:12 +0000

    Debugging

--- intro_debugging/row_sums.c
+++ intro_debugging/row_sums.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+
+int row_sum(int row, int cols)
+{
+    int c;
+    int sum = 0;
+
+    for (c = 1; c <= cols; c++)
+        sum += row * c;
+
+    return sum;
+}
+
 int total_sum(int rows, int cols)
 {
     int r;
@@ -9,4 +22,12 @@ int total_sum(int rows, int cols)
     }
 
     return total;
+}
+
+/* -- DO NOT Modify the code below this line -- */
+
+int main(void)
+{
+    printf("%d\n", total_sum(3, 3));
+    return 0;
 }
