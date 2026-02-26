8aa1cdfef5a9b28b5928fa072c03fcbc318db9f6
OuardaHolberton <12985@holbertonstudents.com>
2026-02-26 15:10:27 +0000

    fix betty

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,15 +9,15 @@ void print_line(int n)
 	int i;
 
 	if (n <= 0)
-    {
+{
 	_putchar('\n');
 	return;
-    }
+}
 
 	for (i = 0; i < n; i++)
-    {
+{
 	_putchar('_');
-    }
+}
 
 	_putchar('\n');
 }
