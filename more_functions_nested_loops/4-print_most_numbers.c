69aa64e92d483d6542654b1dc98ef182f92dd92f
OuardaHolberton <12985@holbertonstudents.com>
2026-02-26 14:49:05 +0000

    task 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - imprime les chiffres de 0 à 9, sauf 2 et 4.
+ *
+ * Description: Utilise _putchar uniquement deux fois.
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	if (i != '2' && i != '4')
+		{
+	_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
+
