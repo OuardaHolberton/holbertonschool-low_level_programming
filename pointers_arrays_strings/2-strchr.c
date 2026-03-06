92db2eafe08a26a3f40f6dd8cdf18a7939deec3e
Ouarda Bouchema <12985@holbertonstudents.com>
2026-03-06 16:39:51 +0100

    _strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,16 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
+	while (1)
 	{
-		if (s[i] == c)
-			return (s + i);
-		i++;
+		if (*s == c)
+			return (s);
+		if (*s == '\0')
+			return (NULL);
+		s++;
 	}
-	if (c == '\0')
-		return (s + i);
-	return (NULL);
 }
