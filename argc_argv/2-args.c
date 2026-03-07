c3ca909b9498d718f4194eb5a73939dedcf714cc
Ouarda Bouchema <12985@holbertonstudents.com>
2026-03-07 23:21:38 +0100

    correction betty

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -10,14 +10,13 @@
  */
 int main(int argc, char *argv[])
 {
-    int i;
-    i =0;
+	int i;
 
-    while (i< argc)
-    {
-        printf("%s\n", argv[i]);
-        i++;
-    }
-    
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 	return (0);
 }
