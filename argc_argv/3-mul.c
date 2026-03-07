e1de5afc97955d9131cd2aed86a463f5f597d736
Ouarda Bouchema <12985@holbertonstudents.com>
2026-03-07 23:19:17 +0100

    correction ajout de bibliotheque

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Multiplies numbers
@@ -12,8 +13,8 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Error\n");
-		return (1);
+        printf("Error\n");
+        return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
