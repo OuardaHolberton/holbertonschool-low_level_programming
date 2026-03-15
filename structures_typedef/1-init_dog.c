b67b3be619ffd6986cda3625a00a7dc292ecc085
Ouarda Bouchema <12985@holbertonstudents.com>
2026-03-15 16:19:46 +0100

    verification du NULL

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 
 /**
  * init_dog - initialise les parametres de la structure
@@ -12,8 +13,12 @@
 
 void init_dog(struct dog  *d, char *name, float age, char *owner)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+    if (d != NULL)
+{
+    d->name = name;
+    d->age = age;
+    d->owner = owner;
+}
+
 }
 
