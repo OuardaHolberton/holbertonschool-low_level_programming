6867358f225713187dd1853babf46a5b00a9b8c7
Ouarda Bouchema <12985@holbertonstudents.com>
2026-03-27 11:34:27 +0100

    struct lists

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,13 +11,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+	unsigned int i;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = strlen(str);
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	new->len = i;
 
 	new->next = *head;
 	*head = new;
