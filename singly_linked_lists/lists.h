d751c4744b16a8ff5598c7dcc132a84adcddce23
Ouarda Bouchema <12985@holbertonstudents.com>
2026-03-27 10:58:17 +0100

    ajout du list len

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
@@ -20,5 +20,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif
