756fc24a0faf2d12dce15062b6257ef63b4b9d0e
OuardaHolberton <12985@holbertonstudents.com>
2026-04-12 12:11:44 +0200

    correction open

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
  * @filename: the name of the file to read
  * @letters: the number of letters to read and print
  *
- * Return: the actual number of letters read and printed, or 0 on failure
+ * Return: actual number of letters read and printed, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -15,18 +15,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	/* open the file in read only mode */
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	/* allocate buffer for letters chars */
 	buf = malloc(sizeof(char) * letters);
 	if (buf == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		close(fd);
+		free(buf);
 		return (0);
 	}
-	/* read letters bytes from file into buffer */
 	r = read(fd, buf, letters);
 	if (r == -1)
 	{
@@ -34,11 +31,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	/* write buffer content to stdout */
 	w = write(STDOUT_FILENO, buf, r);
+	if (w != r)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 	free(buf);
 	close(fd);
-	if (w != r)
-		return (0);
 	return (w);
 }
